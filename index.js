8260135afc9e842ddc90c51d75f7134060855651
missycoder mylan <noreen.taha@outlook.com>
2024-03-19 08:16:13 +0000

    updated customers.hbs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
 require('dotenv').config();
+const { createConnection } = require('mysql2/promise');
 
 let app = express();
 app.set('view engine', 'hbs');
@@ -11,10 +12,27 @@ app.use(express.urlencoded({extended:false}));
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
-app.get('/', (req,res) => {
-    res.send('Hello, World!');
-});
+let connection;
+
+async function main() {
+    const connection = await createConnection({
+        'host': process.env.DB_HOST,
+        'user': process.env.DB_USER,
+        'database': process.env.DB_NAME,
+        'password': process.env.DB_PASSWORD
+    })
+}
+
+app.get('/customers', async (req, res) => {
+    let [customers] = await connection.execute('SELECT * FROM Customers INNER JOIN Companies ON Customers.company_id = Companies.company_id');
+    res.render('customers/index', {
+        'customers': customers
+    })
+})
+
 
 app.listen(3000, ()=>{
     console.log('Server is running')
 });
+
+main();
